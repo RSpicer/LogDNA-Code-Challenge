686205898f21cc4f87d5d880e89cd28d589bed60
Ryan Spicer <rspicer@razegroup.com>
2016-05-28 17:38:12 -0400

    Code base completed. Testing time.

--- searchParse.js
+++ searchParse.js
@@ -9,12 +9,13 @@ function mainHandler (text) {
 		//parse
 	} else {
 		filteredObj = filterOperators(textArr);
+		return parseArr(filteredObj);
 	}	
 }
 
 function filterOperators (textArr) {
 	var filteredArray,
-		filteredObj;
+		filteredObj = {};
 
 	//Check if OR exists in array; else, assume AND.
 	function _checkOperators(textArr) {
@@ -56,9 +57,9 @@ function parseArr (filteredObj) {
 
 	if (filteredObj.isOR === true) {
 		finalObj["$or"] = [];
-		for (var i = 0; i < filteredArr; i++) {
+		for (var i = 0; i < filteredArr.length; i++) {
 			var testParens = filteredArr[i].match(inParens);
-			if (testParens.length !== 0) {
+			if (testParens) {
 				//Filter out ORs and ANDs, as before
 				var tempObj = filterOperators(testParens),
 					tempArr = tempObj.values;
@@ -67,24 +68,24 @@ function parseArr (filteredObj) {
 					for (var x = 0; x < tempArr.length; x++) {
 						var operators = tempArr[x].match(parseOperators),
 							testLen = tempArr[x].match(isLen);
-						if(testLen.length !== 0) {
+						if(testLen) {
 							var lenDigits = testLen[0].substring(4, tempArr[x].length - 1)
 							tempArr[x] = {"$len": lenDigits}
 						}
 
-						if (operators.length > 0) {
+						if (operators) {
 							if (operators[0] === "!") {
 								finalObj["$or"]["$or"].push({"$not": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === ">") {
-								finalObj["$or"]["$or"].push({"$gt": tempArr[x].substring(1, tempArr[x].length});
+								finalObj["$or"]["$or"].push({"$gt": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === "<") {
-								finalObj["$or"]["$or"].push({"$lt": tempArr[x].substring(1, tempArr[x].length});
+								finalObj["$or"]["$or"].push({"$lt": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === "=") {
-								finalObj["$or"]["$or"].push({"$eq": tempArr[x].substring(1, tempArr[x].length});
+								finalObj["$or"]["$or"].push({"$eq": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === ">=") {
-								finalObj["$or"]["$or"].push({"$gte": tempArr[x].substring(2, tempArr[x].length});
+								finalObj["$or"]["$or"].push({"$gte": tempArr[x].substring(2, tempArr[x].length)});
 							} else if (operators[0] === "=<") {
-								finalObj["$or"]["$or"].push({"$lte": tempArr[x].substring(2, tempArr[x].length});
+								finalObj["$or"]["$or"].push({"$lte": tempArr[x].substring(2, tempArr[x].length)});
 							} else {
 								//ERROR
 							}
@@ -97,7 +98,7 @@ function parseArr (filteredObj) {
 					for (var x = 0; x < tempArr.length; x++) {
 						var operators = tempArr[x].match(parseOperators),
 							testLen = tempArr[x].match(isLen);
-						if(testLen.length !== 0) {
+						if(testLen) {
 							var lenDigits = testLen[0].substring(4, tempArr[x].length - 1)
 							tempArr[x] = {"$len": lenDigits}
 						}
@@ -106,15 +107,15 @@ function parseArr (filteredObj) {
 							if (operators[0] === "!") {
 								finalObj["$or"]["$and"].push({"$not": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === ">") {
-								finalObj["$or"]["$and"].push({"$gt": tempArr[x].substring(1, tempArr[x].length});
+								finalObj["$or"]["$and"].push({"$gt": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === "<") {
-								finalObj["$or"]["$and"].push({"$lt": tempArr[x].substring(1, tempArr[x].length});
+								finalObj["$or"]["$and"].push({"$lt": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === "=") {
-								finalObj["$or"]["$and"].push({"$eq": tempArr[x].substring(1, tempArr[x].length});
+								finalObj["$or"]["$and"].push({"$eq": tempArr[x].substring(1, tempArr[x].length)});
 							} else if (operators[0] === ">=") {
-								finalObj["$or"]["$and"].push({"$gte": tempArr[x].substring(2, tempArr[x].length});
+								finalObj["$or"]["$and"].push({"$gte": tempArr[x].substring(2, tempArr[x].length)});
 							} else if (operators[0] === "=<") {
-								finalObj["$or"]["$and"].push({"$lte": tempArr[x].substring(2, tempArr[x].length});
+								finalObj["$or"]["$and"].push({"$lte": tempArr[x].substring(2, tempArr[x].length)});
 							} else {
 								//ERROR
 							}
@@ -125,10 +126,125 @@ function parseArr (filteredObj) {
 				}
 			} else {
 				var operators = filteredArr[i].match(parseOperators),
-					testLen = filteredArr[i]
+					testLen = filteredArr[i].match(isLen);
+
+				if (operators) {
+					if (operators[0] === "!") {
+						finalObj["$or"].push({"$not": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === ">") {
+						finalObj["$or"].push({"$gt": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === "<") {
+						finalObj["$or"].push({"$lt": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === "=") {
+						finalObj["$or"].push({"$eq": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === ">=") {
+						finalObj["$or"].push({"$gte": filteredArr[i].substring(2, filteredArr[i].length)});
+					} else if (operators[0] === "=<") {
+						finalObj["$or"].push({"$lte": filteredArr[i].substring(2, filteredArr[i].length)});
+					} else {
+						//ERROR
+					}
+				} else {
+					finalObj["$or"].push(filteredArr[i]);
+				}
 			}
 		}
 	} else {
 		finalObj["$and"] = [];
+		for (var i = 0; i < filteredArr.length; i++) {
+			var testParens = filteredArr[i].match(inParens);
+			if (testParens) {
+				//Filter out ORs and ANDs, as before
+				var tempObj = filterOperators(testParens),
+					tempArr = tempObj.values;
+				if (tempObj.isOR === true) {
+					finalObj["$and"].push({"$or": []});
+					for (var x = 0; x < tempArr.length; x++) {
+						var operators = tempArr[x].match(parseOperators),
+							testLen = tempArr[x].match(isLen);
+						if(testLen) {
+							var lenDigits = testLen[0].substring(4, tempArr[x].length - 1)
+							tempArr[x] = {"$len": lenDigits}
+						}
+
+						if (operators) {
+							if (operators[0] === "!") {
+								finalObj["$and"]["$or"].push({"$not": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === ">") {
+								finalObj["$and"]["$or"].push({"$gt": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === "<") {
+								finalObj["$and"]["$or"].push({"$lt": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === "=") {
+								finalObj["$and"]["$or"].push({"$eq": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === ">=") {
+								finalObj["$and"]["$or"].push({"$gte": tempArr[x].substring(2, tempArr[x].length)});
+							} else if (operators[0] === "=<") {
+								finalObj["$and"]["$or"].push({"$lte": tempArr[x].substring(2, tempArr[x].length)});
+							} else {
+								//ERROR
+							}
+						} else {
+							finalObj["$and"]["$or"].push(tempArr[x]);
+						}
+					}
+				} else {
+					finalObj["$and"].push({"$and": []});
+					for (var x = 0; x < tempArr.length; x++) {
+						var operators = tempArr[x].match(parseOperators),
+							testLen = tempArr[x].match(isLen);
+						if(testLen) {
+							var lenDigits = testLen[0].substring(4, tempArr[x].length - 1)
+							tempArr[x] = {"$len": lenDigits}
+						}
+
+						if (operators) {
+							if (operators[0] === "!") {
+								finalObj["$and"]["$and"].push({"$not": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === ">") {
+								finalObj["$and"]["$and"].push({"$gt": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === "<") {
+								finalObj["$and"]["$and"].push({"$lt": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === "=") {
+								finalObj["$and"]["$and"].push({"$eq": tempArr[x].substring(1, tempArr[x].length)});
+							} else if (operators[0] === ">=") {
+								finalObj["$and"]["$and"].push({"$gte": tempArr[x].substring(2, tempArr[x].length)});
+							} else if (operators[0] === "=<") {
+								finalObj["$and"]["$and"].push({"$lte": tempArr[x].substring(2, tempArr[x].length)});
+							} else {
+								//ERROR
+							}
+						} else {
+							finalObj["$and"]["$and"].push(tempArr[x]);
+						}
+					}
+				}
+			} else {
+				var operators = filteredArr[i].match(parseOperators),
+					testLen = filteredArr[i].match(isLen);
+
+				if (operators) {
+					if (operators[0] === "!") {
+						finalObj["$and"].push({"$not": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === ">") {
+						finalObj["$and"].push({"$gt": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === "<") {
+						finalObj["$and"].push({"$lt": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === "=") {
+						finalObj["$and"].push({"$eq": filteredArr[i].substring(1, filteredArr[i].length)});
+					} else if (operators[0] === ">=") {
+						finalObj["$and"].push({"$gte": filteredArr[i].substring(2, filteredArr[i].length)});
+					} else if (operators[0] === "=<") {
+						finalObj["$and"].push({"$lte": filteredArr[i].substring(2, filteredArr[i].length)});
+					} else {
+						//ERROR
+					}
+				} else {
+					finalObj["$and"].push(filteredArr[i]);
+				}
+			}
+		}
 	}
+	return finalObj;
 }
+
+console.error(mainHandler("(error OR info) AND (lol OR lel)"));
